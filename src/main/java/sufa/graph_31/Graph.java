3c910633ff7307dc214a17e52db033b10c66eaa0
zhuyanyun <yanyunzhu@newbanker.cn>
2018-12-21 16:47:44 +0800

    图

--- src/main/java/sufa/graph_31/Graph.java
+++ src/main/java/sufa/graph_31/Graph.java
@@ -1,6 +1,7 @@
 package sufa.graph_31;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * @Description TODO
@@ -23,5 +24,39 @@ public class Graph {
     public void addEdge(int s,int t){
         adj[s].add(t);
         adj[t].add(s);
+    }
+
+    public void bfs(int s, int t) {
+        if (s == t) return;
+        boolean[] visited = new boolean[v];
+        visited[s] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
+        int[] prev = new int[v];
+        for (int i = 0; i < v; ++i) {
+            prev[i] = -1;
+        }
+        while (queue.size() != 0) {
+            int w = queue.poll();
+            for (int i = 0; i < adj[w].size(); ++i) {
+                int q = adj[w].get(i);
+                if (!visited[q]) {
+                    prev[q] = w;
+                    if (q == t) {
+                        print(prev, s, t);
+                        return;
+                    }
+                    visited[q] = true;
+                    queue.add(q);
+                }
+            }
+        }
+    }
+
+    private void print(int[] prev, int s, int t) { // 递归打印 s->t 的路径
+        if (prev[t] != -1 && t != s) {
+            print(prev, s, prev[t]);
+        }
+        System.out.print(t + " ");
     }
 }
