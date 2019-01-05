410a361fce6dd8b90b9d64ee83eec749a779bf68
zhuyanyun <yanyunzhu@newbanker.cn>
2019-01-05 18:21:21 +0800

    图，字符串对比

---
+++ src/main/java/sufa/String_34/Kmp.java
@@ -0,0 +1,55 @@
+package sufa.String_34;
+
+/**
+ * @Description TODO
+ * @Author zhuyanyun
+ * @Date 2019-01-04 10:39
+ * @Vertion 1.0
+ **/
+public class Kmp {
+
+    // a, b 分别是主串和模式串；n, m 分别是主串和模式串的长度。
+    public static int kmp(char[] a, int n, char[] b, int m) {
+        int[] next = getNexts(b, m);
+        int j = 0;
+        for (int i = 0; i < n; ++i) {
+            //找到坏字符
+            while (j > 0 && a[i] != b[j]) { // 一直找到 a[i] 和 b[j]
+                j = next[j - 1] + 1;   //（不特别清楚）
+            }
+
+            if (a[i] == b[j]) {
+                ++j;
+            }
+            if (j == m) { // 找到匹配模式串的了
+                return i - m + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String a = "ababaeabac";
+        String b = "ababacd";
+        Kmp kmp = new Kmp();
+        kmp(a.toCharArray(),10, b.toCharArray(), 7);
+    }
+
+
+    // b 表示模式串，m 表示模式串的长度
+    private static int[] getNexts(char[] b, int m) {
+        int[] next = new int[m];
+        next[0] = -1;
+        int k = -1;
+        for (int i = 1; i < m; ++i) {
+            while (k != -1 && b[k + 1] != b[i]) {
+                k = next[k];
+            }
+            if (b[k + 1] == b[i]) {
+                ++k;
+            }
+            next[i] = k;
+        }
+        return next;
+    }
+}
