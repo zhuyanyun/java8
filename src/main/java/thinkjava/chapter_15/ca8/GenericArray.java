86cdd6787625cc327d3db5b8a374e23d0bacadf1
zhuyanyun <yanyunzhu@newbanker.cn>
2019-09-03 20:39:17 +0800

    T

---
+++ src/main/java/thinkjava/chapter_15/ca8/GenericArray.java
@@ -0,0 +1,25 @@
+package thinkjava.chapter_15.ca8;//: generics/GenericArray.java
+
+public class GenericArray<T> {
+    private T[] array;
+//    @SuppressWarnings("unchecked")
+    public GenericArray(int sz) {
+        array = (T[])new Object[sz];
+    }
+
+    //对象数组转型
+    public void put(int index, T item) {
+      array[index] = item;
+    }
+    public T get(int index) { return array[index]; }
+    // Method that exposes the underlying representation:
+    public T[] rep() { return array; }
+
+    public static void main(String[] args) {
+      GenericArray<Integer> gai = new GenericArray<Integer>(10);
+      // This causes a ClassCastException:
+      //! Integer[] ia = gai.rep();
+      // This is OK:
+      Object[] oa = gai.rep();
+    }
+} ///:~
