574b003de6edaab6f5c460d82c734e2066351522
zhuyanyun <yanyunzhu@newbanker.cn>
2019-09-04 11:01:31 +0800

    <?>

--- src/main/java/thinkjava/chapter_15/ca9/BasicBounds.java
+++ src/main/java/thinkjava/chapter_15/ca9/BasicBounds.java
@@ -3,11 +3,11 @@ package thinkjava.chapter_15.ca9;//: generics/BasicBounds.java
 interface HasColor { java.awt.Color getColor(); }
 
 class Colored<T extends HasColor> {
-  T item;
-  Colored(T item) { this.item = item; }
-  T getItem() { return item; }
-  // The bound allows you to call a method:
-  java.awt.Color color() { return item.getColor(); }
+    T item;
+    Colored(T item) { this.item = item; }
+    T getItem() { return item; }
+    // The bound allows you to call a method:
+    java.awt.Color color() { return item.getColor(); }
 }
 
 class Dimension { public int x, y, z; }
@@ -17,13 +17,13 @@ class Dimension { public int x, y, z; }
 	
 // Multiple bounds:
 class ColoredDimension<T extends Dimension & HasColor> {
-  T item;
-  ColoredDimension(T item) { this.item = item; }
-  T getItem() { return item; }
-  java.awt.Color color() { return item.getColor(); }
-  int getX() { return item.x; }
-  int getY() { return item.y; }
-  int getZ() { return item.z; }
+    T item;
+    ColoredDimension(T item) { this.item = item; }
+    T getItem() { return item; }
+    java.awt.Color color() { return item.getColor(); }
+    int getX() { return item.x; }
+    int getY() { return item.y; }
+    int getZ() { return item.z; }
 }
 
 interface Weight { int weight(); }	
@@ -31,28 +31,27 @@ interface Weight { int weight(); }
 // As with inheritance, you can have only one
 // concrete class but multiple interfaces:
 class Solid<T extends Dimension & HasColor & Weight> {
-  T item;
-  Solid(T item) { this.item = item; }
-  T getItem() { return item; }
-  java.awt.Color color() { return item.getColor(); }
-  int getX() { return item.x; }
-  int getY() { return item.y; }
-  int getZ() { return item.z; }
-  int weight() { return item.weight(); }
+    T item;
+    Solid(T item) { this.item = item; }
+    T getItem() { return item; }
+    java.awt.Color color() { return item.getColor(); }
+    int getX() { return item.x; }
+    int getY() { return item.y; }
+    int getZ() { return item.z; }
+    int weight() { return item.weight(); }
 }
 
-class Bounded
-extends Dimension implements HasColor, Weight {
+class Bounded extends Dimension implements HasColor, Weight {
   public java.awt.Color getColor() { return null; }
   public int weight() { return 0; }
 }	
 
 public class BasicBounds {
   public static void main(String[] args) {
-    Solid<Bounded> solid =
-      new Solid<Bounded>(new Bounded());
-    solid.color();
-    solid.getY();
-    solid.weight();
+      Solid<Bounded> solid = new Solid<Bounded>(new Bounded());
+      solid.color();
+      solid.getY();
+      solid.weight();
+    System.out.println(solid.color());
   }
 } ///:~
---
+++ src/main/java/thinkjava/chapter_15/cas10/CovariantArrays.java
@@ -0,0 +1,35 @@
+package thinkjava.chapter_15.cas10;//: generics/CovariantArrays.java
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Fruit {}
+class Apple extends Fruit {}
+class Jonathan extends Apple {}
+class Orange extends Fruit {}
+
+public class CovariantArrays {
+    public static void main(String[] args) {
+        Fruit[] fruit = new Apple[10];
+        fruit[0] = new Apple(); // OK
+        fruit[1] = new Jonathan(); // OK
+        // Runtime type is Apple[], not Fruit[] or Orange[]:
+        try {
+          // Compiler allows you to add Fruit:
+          fruit[0] = new Fruit(); // ArrayStoreException
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+        try {
+          // Compiler allows you to add Oranges:
+          fruit[0] = new Orange(); // ArrayStoreException
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+    }
+
+//    List<Fruit> flist = new ArrayList<Apple>();
+} /* Output:
+java.lang.ArrayStoreException: Fruit
+java.lang.ArrayStoreException: Orange
+*///:~
